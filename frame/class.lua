557895ca91ac5581e0f973684d29c732d5a22a5c
upupzealot <hzxjhspxn@163.com>
2016-09-25 16:19:50 +0800

    fix

--- frame/class.lua
+++ frame/class.lua
@@ -1,5 +1,5 @@
 -- class.lua
--- Compatible with Lua 5.1 (not 5.0).
+-- 为Lua提供类、继承等概念
 function class(base, init)
     local c = {}    -- a new class instance
     if not init and type(base) == 'function' then
@@ -7,7 +7,7 @@ function class(base, init)
         base = nil
     elseif type(base) == 'table' then
         -- our new class is a shallow copy of the base class!
-        for i,v in pairs(base) do
+        for i, v in pairs(base) do
             c[i] = v
         end
         c._base = base
@@ -20,9 +20,9 @@ function class(base, init)
     local mt = {}
     mt.__call = function(class_tbl, ...)
         local obj = {}
-        setmetatable(obj,c)
+        setmetatable(obj, c)
         if class_tbl.init then
-            class_tbl.init(obj,...)
+            class_tbl.init(obj, ...)
         else
             -- make sure that any stuff from the base class is initialized!
             if base and base.init then
