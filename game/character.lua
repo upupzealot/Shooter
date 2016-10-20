b82e9549f1ce5815927c62887895a7c4fc741dca
wb-pxn234203 <wb-pxn234203@alibaba-inc.com>
2016-10-20 15:27:52 +0800

    Character外形

--- game/character.lua
+++ game/character.lua
@@ -9,8 +9,10 @@ end
 
 -- 绘制一个直径为15的圆
 function Character:render()
-  love.graphics.push()
+  love.graphics.push('all')
     love.graphics.setColor(self.color.r, self.color.g, self.color.b)
-    love.graphics.ellipse('fill', self.pos.x, self.pos.y, self.size, self.size, 36)
+    love.graphics.setLineWidth(4)
+    --love.graphics.ellipse('fill', self.pos.x, self.pos.y, self.size - 8, self.size - 8, 24)
+    love.graphics.ellipse('line', self.pos.x, self.pos.y, self.size - 2, self.size - 2, 24)
   love.graphics.pop()
 end
