b1b34f8f00845ec62d431b16f58fb3b2c3617497
wb-pxn234203 <wb-pxn234203@alibaba-inc.com>
2016-11-02 19:33:18 +0800

    Enemy的移动

--- game/enemy.lua
+++ game/enemy.lua
@@ -2,6 +2,10 @@ Enemy = class(Character, 'Enemy')
 
 function Enemy:init(pos)
   Character.init(self, pos)
-
   self.color = {r = 255, g = 125, b = 125}
+
+  self.navigator = NearestNavigator(self, {target_class = 'Player'})
+  self.speed = 50
+  self.direction = vec2(0, 0)
+  self.mover = Mover(self)
 end
