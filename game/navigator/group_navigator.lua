66b240324c8ab44168eaf549b8fe901f8944a5f7
wb-pxn234203 <wb-pxn234203@alibaba-inc.com>
2016-11-16 13:27:34 +0800

    fix

--- game/navigator/group_navigator.lua
+++ game/navigator/group_navigator.lua
@@ -1,7 +1,5 @@
 GroupNavigator = class(Navigator, 'GroupNavigator')
 
-GroupNavigator.ZERO = vec2(0, 0)
-
 function GroupNavigator:navigate(dt)
   local result_direction = vec2(0, 0)
 
@@ -11,6 +9,6 @@ function GroupNavigator:navigate(dt)
       result_direction = result_direction + navigator.direction * navigator.weight
     end
   end
-  
+
   self.direction = result_direction:directionalize()
 end
